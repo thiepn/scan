12ac504134e98ef5adab707e87f69045df2f8ba1
thiepn <junsosmart@gmail.com>
2026-09-25 19:01:42 +0200

    Configure Room schema export

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
+    id("androidx.room")
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 android {
